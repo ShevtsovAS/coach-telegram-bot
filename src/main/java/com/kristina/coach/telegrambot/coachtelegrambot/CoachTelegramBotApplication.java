72831183729f3796aa1111a68c6ffcc260f18e34
Aleksandr Shevtsov <hack85sh@gmail.com>
2024-04-29 15:24:20 -0400

    init commit

---
+++ src/main/java/com/kristina/coach/telegrambot/coachtelegrambot/CoachTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.kristina.coach.telegrambot.coachtelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoachTelegramBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kristina/coach/telegrambot/coachtelegrambot/CoachTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kristina.coach.telegrambot.coachtelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoachTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
