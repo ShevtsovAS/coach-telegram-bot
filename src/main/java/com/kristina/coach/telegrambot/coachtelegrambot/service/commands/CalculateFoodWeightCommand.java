91e7b39ce357198cc08a31900c14feb135e3321f
Aleksandr Shevtsov <hack85sh@gmail.com>
2024-05-06 18:05:01 -0400

    intro for calculate food weight added

--- src/main/java/com/kristina/coach/telegrambot/coachtelegrambot/service/commands/CalculateFoodWeightCommand.java
+++ src/main/java/com/kristina/coach/telegrambot/coachtelegrambot/service/commands/CalculateFoodWeightCommand.java
@@ -2,6 +2,7 @@ package com.kristina.coach.telegrambot.coachtelegrambot.service.commands;
 
 import com.kristina.coach.telegrambot.coachtelegrambot.service.CoachTelegramBot;
 import com.kristina.coach.telegrambot.coachtelegrambot.service.steps.calculate_food_weight.CalcFoodWeightSteps;
+import com.kristina.coach.telegrambot.coachtelegrambot.util.BotUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +14,7 @@ public final class CalculateFoodWeightCommand implements CommandExecutor {
 
     @Override
     public void execute(CoachTelegramBot bot) {
+        bot.sendTextMessage(BotUtil.getMessageFromResource("calculate-food-weight-intro"));
         var calcFoodWeightStep = CalcFoodWeightSteps.getStart();
         bot.setCurrentStep(calcFoodWeightStep.getStepExecutorName());
         bot.sendTextMessage(calcFoodWeightStep.getMessage(), calcFoodWeightStep.getReplyKeyboard());
